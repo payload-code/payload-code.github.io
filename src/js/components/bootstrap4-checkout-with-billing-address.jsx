79234d56cc3f699123cf39d55f96a00a767e3a5b
NathanYearout <nathanyearout@gmail.com>
2023-11-16 10:16:57 -0500

    Further changes to bootstrap checkout

---
+++ src/js/components/bootstrap4-checkout-with-billing-address.jsx
@@ -0,0 +1,15 @@
+Payload('test_client_key_3btrkEyC6xvcByXLthuZx')
+
+var checkout_form = new Payload.Form({
+    form: $('#checkout-form').get(0),
+    styles: { invalid: 'is-invalid' },
+    autosubmit: false
+})
+
+checkout_form.on('processed', function(data) {
+    $('#paid-modal').modal('show')
+})
+
+checkout_form.on('declined', function(data) {
+    $('#declined-modal').modal('show')
+})

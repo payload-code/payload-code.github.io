2fae9842688ee56ef61c3d4ddf2981bd9cbbf1d1
Nathan Yearout <nathanyearout@gmail.com>
2023-11-20 12:18:23 -0500

    Added title and protip to material-ui-add-payment-details.jsx

--- src/js/components/material-ui-add-payment-details.jsx
+++ src/js/components/material-ui-add-payment-details.jsx
@@ -37,6 +37,29 @@ function updateClasses(element, isFocused) {
     }
 }
 
+function ProTip() {
+    return (
+        <div>
+            <Typography color="textSecondary" style={{marginTop: 24,}}>
+                For more information on integrating Payload: See the {' '}
+                <Link href="https://github.com/payload-code/payload-react">
+                    payload-react
+                </Link>{' '}
+                repo on GitHub and the {' '}
+                <Link href="https://docs.payload.co">Payload Documentation</Link>.
+            </Typography>
+            <Typography color="textSecondary" style={{marginTop: 24,}}>
+                See the {' '}
+                <Link
+                    href="https://github.com/payload-code/payload-code.github.io/blob/master/material-ui-add-payment-details.html">
+                    source code
+                </Link>{' '}
+                for this demo on GitHub.
+            </Typography>
+        </div>
+    );
+}
+
 function PaymentMethod() {
     const [open, setOpen] = React.useState(false);
     const [disabled, setDisabled] = React.useState(false);
@@ -54,97 +77,104 @@ function PaymentMethod() {
     };
 
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-        }}>
-            <PayloadForm
-                formId="checkout-form"
-                className="container"
-                clientToken='test_client_key_3btrkEyC6xvcByXLthuZx'
-                onSubmit={handleSubmit}
-                ref={ref}
-                autoSubmit={false}
-            >
-                <PayloadInput type="hidden" value="card"/>
-                <div>
-                    <FormControl>
-                        <InputLabel>Card Number</InputLabel>
-                        <div
-                            className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
-                            style={{width: '15rem'}}
-                            onFocus={(evt) => updateClasses(evt.target, true)}
-                            onBlur={(evt) => updateClasses(evt.target, false)}
-                        >
-                            <PayloadInput
-                                className="MuiInputBase-input MuiInput-input"
-                                type="card_number"
-                                placeholder=""
-                            />
-                        </div>
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Expires</InputLabel>
-                        <div
-                            className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
-                            style={{width: '15rem'}}
-                            onFocus={(evt) => updateClasses(evt.target, true)}
-                            onBlur={(evt) => updateClasses(evt.target, false)}
-                        >
-                            <PayloadInput
-                                className="MuiInputBase-input MuiInput-input"
-                                type="expiry"
-                                placeholder=""
-                            />
-                        </div>
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>CVC</InputLabel>
-                        <div
-                            className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
-                            style={{width: '4rem'}}
-                            onFocus={(evt) => updateClasses(evt.target, true)}
-                            onBlur={(evt) => updateClasses(evt.target, false)}
-                        >
-                            <PayloadInput
-                                className="MuiInputBase-input MuiInput-input"
-                                type="card_code"
-                                placeholder=""
-                            />
-                        </div>
-                    </FormControl>
-                </div>
-                <div>
-                    <FormControl style={{width: '23rem'}}>
-                        <InputLabel htmlFor="my-input">Account Holder</InputLabel>
-                        <Input id="my-input" aria-describedby="my-helper-text"
-                               inputProps={{"pl-input": "account_holder"}}/>
-                    </FormControl>
-                </div>
-                <div style={{marginTop: '1rem'}}>
-                    <FormControl>
-                        <Button variant="contained" color="primary" type="submit" disabled={disabled}>Add Card</Button>
-                    </FormControl>
-                </div>
-                <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-                    <DialogTitle id="alert-dialog-title" color="primary">Your Payment Method Was Added!</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            Thanks for trying out Payload
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose} color="primary" autoFocus>
-                            Close
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </PayloadForm>
-        </div>
+        <PayloadForm
+            formId="checkout-form"
+            className="container"
+            clientToken='test_client_key_3btrkEyC6xvcByXLthuZx'
+            onSubmit={handleSubmit}
+            ref={ref}
+            autoSubmit={false}
+        >
+            <PayloadInput type="hidden" value="card"/>
+            <div>
+                <FormControl>
+                    <InputLabel>Card Number</InputLabel>
+                    <div
+                        className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
+                        style={{width: '15rem'}}
+                        onFocus={(evt) => updateClasses(evt.target, true)}
+                        onBlur={(evt) => updateClasses(evt.target, false)}
+                    >
+                        <PayloadInput
+                            className="MuiInputBase-input MuiInput-input"
+                            type="card_number"
+                            placeholder=""
+                        />
+                    </div>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Expires</InputLabel>
+                    <div
+                        className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
+                        style={{width: '15rem'}}
+                        onFocus={(evt) => updateClasses(evt.target, true)}
+                        onBlur={(evt) => updateClasses(evt.target, false)}
+                    >
+                        <PayloadInput
+                            className="MuiInputBase-input MuiInput-input"
+                            type="expiry"
+                            placeholder=""
+                        />
+                    </div>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>CVC</InputLabel>
+                    <div
+                        className="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-formControl MuiInput-formControl"
+                        style={{width: '4rem'}}
+                        onFocus={(evt) => updateClasses(evt.target, true)}
+                        onBlur={(evt) => updateClasses(evt.target, false)}
+                    >
+                        <PayloadInput
+                            className="MuiInputBase-input MuiInput-input"
+                            type="card_code"
+                            placeholder=""
+                        />
+                    </div>
+                </FormControl>
+            </div>
+            <div>
+                <FormControl style={{width: '23rem'}}>
+                    <InputLabel htmlFor="my-input">Account Holder</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text"
+                           inputProps={{"pl-input": "account_holder"}}/>
+                </FormControl>
+            </div>
+            <div style={{marginTop: '1rem'}}>
+                <FormControl>
+                    <Button variant="contained" color="primary" type="submit" disabled={disabled}>Add Card</Button>
+                </FormControl>
+            </div>
+            <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title" color="primary">Your Payment Method Was Added!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Thanks for trying out Payload
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </PayloadForm>
     );
 }
 
-ReactDOM.render(<PaymentMethod/>, document.getElementById('root'));
+function App() {
+    return (
+        <Container maxWidth="xs">
+            <div style={{marginTop: 24,}}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Add Payment Details
+                </Typography>
+                <PaymentMethod/>
+                <ProTip/>
+            </div>
+        </Container>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 

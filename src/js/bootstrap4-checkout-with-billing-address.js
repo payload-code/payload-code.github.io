515f4f3f1000d50e31360155ba4884ca04a3f432
Nathan Yearout <nathanyearout@gmail.com>
2024-10-16 12:03:20 -0400

    Doc Changes for preventDefaultOnSubmit and preventSubmitOnEnter (#17)

--- src/js/bootstrap4-checkout-with-billing-address.js
+++ src/js/bootstrap4-checkout-with-billing-address.js
@@ -3,7 +3,7 @@ Payload('test_client_key_3btrkEyC6xvcByXLthuZx')
 const checkoutForm = new Payload.Form({
   form: document.getElementById('checkout-form'),
   styles: { invalid: 'is-invalid' },
-  autosubmit: false,
+  preventDefaultOnSubmit: true,
 })
   .on('processed', function (data) {
     $('#paid-modal').modal('show')
--- src/js/plaid-example.js
+++ src/js/plaid-example.js
@@ -3,7 +3,7 @@ Payload('test_client_key_3bcr16ohAy8aEcwK3Vffs')
 const checkout_form = new Payload.Form({
   form: $('#checkout-form').get(0),
   styles: { invalid: 'is-invalid' },
-  autosubmit: false,
+  preventDefaultOnSubmit: true,
   payment: {
     processing_id: 'acct_3bcr6L5qxxnXJjNwu4MHg',
   },
